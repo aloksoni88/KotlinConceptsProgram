2f984397a5c7f6c396a604ae132776d948e26698
Alok Soni <alok.bist2010@gmail.com>
2023-05-28 02:41:46 +0530

    Added very first commit regarding kotlin concetps program

---
+++ src/main/kotlin/02_explore_first_app.kt
@@ -0,0 +1,22 @@
+
+// Explore First App
+
+fun exploreFirstApp() {
+
+    println("Hello World")
+
+    println(10)
+
+    println(true)
+
+    println(10 / 2)
+
+    println(94.2)
+
+    println(9 - 3)
+
+}
+
+fun main() {
+    exploreFirstApp()
+}
